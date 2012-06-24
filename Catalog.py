a5024f0b53b4fe362e22b61d5c9d7877c92c724b
jayfro <lilclinx@yahoo.co.nz>
2012-06-24 12:42:28 +0000

    {{Day4}}

---
+++ Catalog.py
@@ -0,0 +1,27 @@
+#LAB 04: Data Structures
+
+# Question 2
+
+# a.
+
+# I would use a dictionary 
+
+# b.
+
+catalog_dict = { 'Apples': 7.3, 'Bananas' : 5.5, 'Bread': 1.0, 'Carrots': 10.0, 'Champagne' : 20.90, 'Strawberries': 32.6 }
+print
+print
+
+# c.
+
+# price change
+
+catalog_dict['Strawberries'] = 63.43
+print
+print
+
+# d.
+
+#  new products
+
+catalog_dict['Chicken'] = 6.5
